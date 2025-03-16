7c201e5515ee169d9c2eeee78521ad8cfd0aebbd
Pascal Janetzky <pascal.janetzky@stud-mail.uni-wuerzburg.de>
2025-03-16 15:51:08 +0100

    Added billion-scale

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
           bar.style.height = height + "px";
           bar.dataset.scaled = "true";
 
-          if (bar.classList.contains("ruler")) {
+            if (bar.classList.contains("ruler")) {
             const markerSpacing = 10000 / width // 1pixel = €1000, so 1M = 1000 pixels
             const markerCount = 1000 // goes up to 10 billion, or 10000 million
             for (let i = 1; i <= markerCount; i++) { // Place a marker every 1 million
@@ -21,16 +21,41 @@ document.addEventListener("DOMContentLoaded", function() {
               marker.classList.add("marker");
               const markerPosition = i * markerSpacing;
               marker.style.top = markerPosition + "px";
-          
+            
               const label = document.createElement("div");
               label.classList.add("marker-label");
               label.style.top = markerPosition + "px";
               label.innerText = `€${i*10} M`;
-          
+            
               bar.appendChild(marker);
               bar.appendChild(label);
             }
+
+            const billionMarkerSpacing = (1000000000 / 1000) / width // 1pixel = €1000, so 1B = 1000000 pixels
+            const billionMarkerCount = 500 // goes up to 10 billion
+            for (let i = 1; i <= billionMarkerCount; i++) { // Place a marker every 1 billion
+
+
+              const billionMarkerPosition = i * billionMarkerSpacing;
+              if (billionMarkerPosition < height) {
+                const billionMarker = document.createElement("div");
+                billionMarker.classList.add("billion-marker");
+
+                billionMarker.style.top = billionMarkerPosition + "px";
+              
+                const billionLabel = document.createElement("div");
+                billionLabel.classList.add("billion-marker-label");
+                billionLabel.style.top = billionMarkerPosition + "px";
+                billionLabel.innerText = `€${i} B`;
+              
+                bar.appendChild(billionMarker);
+                bar.appendChild(billionLabel);
+
+            }
+            }
           }
+
+
           
         }
       });
